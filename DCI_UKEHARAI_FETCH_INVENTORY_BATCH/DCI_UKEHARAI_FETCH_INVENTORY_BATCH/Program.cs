b8a0b633e7edc9fc63c680eeadff38879212c1e8
github-devmaker <peerapong.kiadtipongwiriya@gmail.com>
2024-04-25 17:15:21 +0700

    init
    init

---
+++ DCI_UKEHARAI_FETCH_INVENTORY_BATCH/DCI_UKEHARAI_FETCH_INVENTORY_BATCH/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCI_UKEHARAI_FETCH_INVENTORY_BATCH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
